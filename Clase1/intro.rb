2e931b620d77836c8c840a167b19097eb7285381
Pablo <pablo.um00@gmail.com>
2018-07-17 19:01:25 -0500

    Creación de carpeta con ejercicios de clase

---
+++ Clase1/intro.rb
@@ -0,0 +1,11 @@
+# x = 1; y = 2; z = 3; w = 4;
+#
+# p x*y; p y+z; p z-w; p w/1;
+
+## Documentación oficial: ruby-doc.com
+## Librería oficial
+
+p "Hola, señor usaurio. Ingrese por favor su nombre"
+name = gets.chomp
+
+p "Hola, #{name}"
