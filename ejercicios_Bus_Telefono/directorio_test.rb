9faf73dfc5e994c96fb39939e5b8f374a7e03982
Pablo <pablo.um00@gmail.com>
2018-10-24 10:39:24 -0500

    Back up

--- ejercicios_Bus_Telefono/directorio_test.rb
+++ ejercicios_Bus_Telefono/directorio_test.rb
@@ -7,6 +7,10 @@ class TelefonoTest < Minitest::Test
     @iphone = Telefono.new
   end
 
+  def test_class_of_instance
+    assert_instance_of(Telefono, @iphone)
+  end
+
   def test_llamar
     assert_equal false,@iphone.llamar("Juan",123)
     assert_equal true,@iphone.llamar("Yerry Mina","1194")
